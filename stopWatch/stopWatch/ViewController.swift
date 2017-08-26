a0c65a6b9ba0d119a2979fc0510f04a918c4c7bb
Jonny Holupchinski <jonholup@gmail.com>
2017-08-26 16:47:55 -0500

    basic timer

--- stopWatch/stopWatch/ViewController.swift
+++ stopWatch/stopWatch/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time = 0
+    var stopWatch = Timer()
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func start(_ sender: Any) {
+        stopWatch = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.tick), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        stopWatch.invalidate()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        stopWatch.invalidate()
+        time = 0
+        label.text = String(0)
+    }
+    
+    func tick() {
+        time += 1
+        label.text = String(time)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
